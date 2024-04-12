79b3056d1227b2ec033ccf2935c1edb9e4d3a362
blessedmadukoma <blessedmadukoma@gmail.com>
2024-04-12 12:56:51 +0100

    insert static data to postgres

--- tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-
-    conn = psycopg2.connect(f"host={os.getenv('POSTGRES_HOST')} dbname={os.getenv('POSTGRES_DB')} user={os.getenv('POSTGRES_USER')} password={os.getenv('POSTGRES_PASSWORD')} port={os.getenv('POSTGRES_PORT')}")
-except psycopg2.Error as e:
-    print("Error: could not connect to postgres database ->", e)
